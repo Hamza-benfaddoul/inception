958100422354c31379bc1c0d89521c3a784d0f52
Hamza Benfaddoul <hbenfadd@c3r4p7.1337.ma>
2023-08-17 22:29:14 +0100

    update init.sql

--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -1,7 +1,4 @@
 CREATE DATABASE wordpress;
-CREATE USER 'hbenfadd'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'hbenfadd'@'localhost';
-
-GRANT ALL PRIVILEGES ON *.* TO 'hbenfadd'@'wordpress.srcs_inception' IDENTIFIED BY 'root';
-
+CREATE USER 'hbenfadd'@'wordpress.srcs_inception' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'hbenfadd'@'wordpress.srcs_inception' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
